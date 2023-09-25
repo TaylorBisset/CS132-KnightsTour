1853d5fc6152e1aab09b579ef90e42c5761f1803
Taylor Bisset <taylorbisset@gmail.com>
2023-09-25 13:09:43 -0700

    Comment in assignment header

---
+++ CS132-KnightsTour/main.cpp
@@ -0,0 +1,15 @@
+/*
+ Author:        Taylor Bisset
+ Section:       A
+ Assignment:    P1
+ Description:   A Knight's Tour is a sequence of moves around a chessboard 
+                so that a knight visits every location. 
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
