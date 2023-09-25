ecabda2e5122750af20a10f334da1e152ed3b804
Taylor Bisset <taylorbisset@gmail.com>
2023-09-25 15:29:18 -0700

    Create printBoard function

--- CS132-KnightsTour/main.cpp
+++ CS132-KnightsTour/main.cpp
@@ -1,15 +1,34 @@
 /*
  Author:        Taylor Bisset
  Section:       A
- Assignment:    P1
- Description:   A Knight's Tour is a sequence of moves around a chessboard 
+ Assignment:    Knights Tour
+ Description:   Knight's Tour is a sequence of moves around a chessboard 
                 so that a knight visits every location. 
 */
 #include <iostream>
 
 using namespace std;
 
+// create board size
+const int boardSize = 5; 
+
+// set characters used in printing the board 
+char horLine = 196, verLine = 179, spaceDot = 250;
+
+void printBoard(int board[boardSize][boardSize])
+{
+    for (int row = 0; row < boardSize; row++)
+    {
+        for (int col = 0; col < boardSize; col++)
+        {
+            cout << spaceDot;
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
-    cout << "Hello World!\n";
+    int chessboard[boardSize][boardSize];
+    printBoard(chessboard);
 }
